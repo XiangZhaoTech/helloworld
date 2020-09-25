760dd32ff4226bfb9b6f9c97cccaab7bdc67875f
zhaoxiang020 <zhaoxiang020@ke.com>
2020-09-25 15:28:25 +0800

    first commit

---
+++ src/main/java/com/example/web/HelloController.java
@@ -0,0 +1,28 @@
+package com.example.web;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhaoxiang
+ * @date 2020/04/11
+ */
+@RestController
+public class HelloController {
+    private final Logger logger = Logger.getLogger(getClass());
+
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String index(){
+        ServiceInstance instance = client.getLocalServiceInstance();
+        logger.info("/hello, host:" + instance.getHost() + ", service_id:" + instance.getServiceId());
+        return "Hello World";
+    }
+}
